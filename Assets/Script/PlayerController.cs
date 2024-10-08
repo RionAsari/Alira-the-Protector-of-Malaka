b5abe8c96caa6d4db9256ceb1c8387ae7e95cee5
Riun <rionsen157@gmail.com>
2024-10-08 21:24:31 +0700

    animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
@@ -15,23 +11,33 @@ public class PlayerController : MonoBehaviour
     private bool facingRight = true; // To track which direction player is facing
     private Camera mainCamera;
 
+    // Referensi ke Animator dan Bow Transform
+    private Animator animator;
+    public GameObject bowTransform; // Drag your bowTransform GameObject here in the inspector
+
+    private float moveInput;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         mainCamera = Camera.main;
+
+        // Mendapatkan komponen Animator
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Move();
         HandleJump();
-        AimAtMouse(); // Still keep the function to face the cursor
+        AimAtMouse();
+        UpdateAnimation(); // Panggil untuk update animasi
     }
 
     // Move using A and D keys
     private void Move()
     {
-        float moveInput = Input.GetAxisRaw("Horizontal"); // A = -1, D = 1
+        moveInput = Input.GetAxisRaw("Horizontal"); // A = -1, D = 1
 
         Vector3 moveVelocity = new Vector3(moveInput * moveSpeed, rb.velocity.y, 0);
         rb.velocity = moveVelocity;
@@ -41,6 +47,16 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        // Show or hide the bow based on movement
+        bowTransform.SetActive(moveInput != 0); // Hide if not moving
+    }
+
+    // Update animasi isMoving
+    private void UpdateAnimation()
+    {
+        // Update parameter animasi isMoving
+        animator.SetBool("isMoving", moveInput != 0);
     }
 
     // Aim at the mouse position (only for character rotation)
@@ -77,13 +93,13 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scale;
     }
 
-    // Handle jump input with space
+    // Handle jump input with W
     private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            isGrounded = false; // Jump once when space is pressed
+            isGrounded = false; // Jump once when W is pressed
         }
     }
 
