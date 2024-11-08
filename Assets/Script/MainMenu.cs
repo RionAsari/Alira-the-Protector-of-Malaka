3bca1736013018b96cb6c447e5e478dd01bc6f28
Riun <rionsen157@gmail.com>
2024-11-08 19:19:18 +0700

    fix footstep sound

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,38 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;  // Add this for Slider and UI components
 
 public class MainMenuScript : MonoBehaviour
 {
     public GameObject settingsPanel;
     public GameObject mainMenuPanel;
-    public Slider volumeSlider; // Reference to the slider for volume control
-
-    // Set initial volume on start (you can set this from PlayerPrefs if you want to save the volume setting)
-    private void Start()
-    {
-        if (PlayerPrefs.HasKey("MusicVolume"))
-        {
-            float savedVolume = PlayerPrefs.GetFloat("MusicVolume");
-            AudioListener.volume = savedVolume;
-            volumeSlider.value = savedVolume;
-        }
-        else
-        {
-            AudioListener.volume = 1f; // Default volume
-            volumeSlider.value = 1f; // Default slider position
-        }
-
-        // Add listener for slider value change
-        volumeSlider.onValueChanged.AddListener(SetVolume);
-    }
-
-    // Function to change volume when slider value is adjusted
-    public void SetVolume(float volume)
-    {
-        AudioListener.volume = volume;
-        PlayerPrefs.SetFloat("MusicVolume", volume); // Save volume setting for next time
-    }
 
     public void StartGame()
     {
